71b4905abe49f31418e87eb73628e130d1f3ea9a
Allan Guerreiro <allan.guerreiro@live.com>
2023-05-26 21:59:46 -0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,12 @@
+netsh advfirewall firewall add rule name='ICMP Allow incoming V4 echo request' protocol=icmpv4:8,any dir=in action=allow
+netsh advfirewall firewall add rule name='ICMP Allow incoming V6 echo request' protocol=icmpv6:8,any dir=in action=allow
+
 Import-Module ServerManager
 Install-WindowsFeature RDS-Connection-Broker
 Install-WindowsFeature RDS-Gateway
 Install-WindowsFeature RDS-RD-Server
-nstall-WindowsFeature RDS-Web-Access
+Install-WindowsFeature RDS-Web-Access
 Install-WindowsFeature RSAT-RDS-Tools
+
+shutdown -r -t 10
+exit 0
