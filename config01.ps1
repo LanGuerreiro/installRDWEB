b911ef8b2d8b44005cd4906efe86c3b8644b027a
Allan Guerreiro <allan.guerreiro@live.com>
2023-05-27 12:21:29 -0300

    Update config01.ps1

--- config01.ps1
+++ config01.ps1
@@ -12,3 +12,28 @@ New-RDSessionDeployment -ConnectionBroker  "$myFQDN" -WebAccessServer "$myFQDN" 
 
 Stop-Transcript
 exit 0
+
+
+$servername = 'localhost'
+
+Invoke-Command -ComputerName $servername -ScriptBlock { 
+     if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { return $true }
+     if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { return $true }
+     if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { return $true }
+     try { 
+       $util = [wmiclass]"\\.\root\ccm\clientsdk:CCM_ClientUtilities"
+       $status = $util.DetermineIfRebootPending()
+       if(($status -ne $null) -and $status.RebootPending){
+         return $true
+		 
+shutdown -r -t 10 -f
+Stop-Transcript
+exit 0
+		 
+       }
+     }catch{}
+ 
+     return $false
+Write-Host "Nada"
+
+}
