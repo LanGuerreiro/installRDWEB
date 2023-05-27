9dfec585b147878aa0719fa461f4fa62a70c0ef7
Allan Guerreiro <allan.guerreiro@live.com>
2023-05-27 11:49:04 -0300

    Update config01.ps1

--- config01.ps1
+++ config01.ps1
@@ -1,5 +1,14 @@
+Start-Transcript -Path c:\Script\RDPWEB\config01.log
+
+Write-Host "Remove taskschedule conf01" -ForegroundColor Green
+Unregister-ScheduledTask -TaskName RDPWEB-CONF01 -Confirm:$false
+
 Write-Host "Set ConnectionBroker WebAccessServer SessionHost" -ForegroundColor Green
 $myFQDN=(Get-WmiObject win32_computersystem).DNSHostName+'.'+(Get-WmiObject win32_computersystem).Domain
 
 Write-Host "FQDN: $myFQDN" -ForegroundColor Green
 New-RDSessionDeployment -ConnectionBroker  "$myFQDN" -WebAccessServer "$myFQDN" -SessionHost "$myFQDN"
+
+
+Stop-Transcript
+exit 0
