a835923aadfdbd7429da5bad0d29f307dcc1451a
Allan Guerreiro <allan.guerreiro@live.com>
2023-05-26 21:58:48 -0300

    Create install.ps1

---
+++ install.ps1
@@ -0,0 +1,6 @@
+Import-Module ServerManager
+Install-WindowsFeature RDS-Connection-Broker
+Install-WindowsFeature RDS-Gateway
+Install-WindowsFeature RDS-RD-Server
+nstall-WindowsFeature RDS-Web-Access
+Install-WindowsFeature RSAT-RDS-Tools
