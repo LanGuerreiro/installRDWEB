8cf5b45ac9fceb9e1b960e48aa9d028ce2123a6f
Allan Guerreiro <allan.guerreiro@live.com>
2023-05-26 22:09:49 -0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,3 +1,5 @@
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted
+
 netsh advfirewall firewall add rule name='ICMP Allow incoming V4 echo request' protocol=icmpv4:8,any dir=in action=allow
 netsh advfirewall firewall add rule name='ICMP Allow incoming V6 echo request' protocol=icmpv6:8,any dir=in action=allow
 
