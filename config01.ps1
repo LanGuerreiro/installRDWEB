01200c37eb37813b412d1f7c430d65e9a2c8ebe8
Allan Guerreiro <allan.guerreiro@live.com>
2023-05-27 16:27:09 -0300

    Update config01.ps1

--- config01.ps1
+++ config01.ps1
@@ -4,32 +4,11 @@ Start-Transcript -Path c:\Script\RDPWEB\config01-$date.log
 $myFQDN=(Get-WmiObject win32_computersystem).DNSHostName+'.'+(Get-WmiObject win32_computersystem).Domain
 Write-Host "FQDN: $myFQDN" -ForegroundColor Green
 
-#Start-Sleep -Seconds 120
+Start-Sleep -Seconds 120
 
-Invoke-Command -ComputerName $myFQDN -ScriptBlock { 
-     if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { return $true }
-     if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { return $true }
-     if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { return $true }
-     try { 
-       $util = [wmiclass]"\\.\root\ccm\clientsdk:CCM_ClientUtilities"
-       $status = $util.DetermineIfRebootPending()
-       if(($status -ne $null) -and $status.RebootPending){
+Write-Host "Remove taskschedule conf01" -ForegroundColor Green 
+Unregister-ScheduledTask -TaskName RDPWEB-CONF01 -Confirm:$false
 
-	Write-Host "Reboot now"        
-        shutdown -r -t 0 -f
- 	return $true
-		 
-		 
-       }
-     }catch{ 
-     		Write-Host "without pending boot"
- 		Write-Host "Remove taskschedule conf01" -ForegroundColor Green 
- 		#Unregister-ScheduledTask -TaskName RDPWEB-CONF01 -Confirm:$false
- }
-}
-
-$myFQDN=(Get-WmiObject win32_computersystem).DNSHostName+'.'+(Get-WmiObject win32_computersystem).Domain
-Write-Host "FQDN: $myFQDN" -ForegroundColor Green
 Write-Host "Set ConnectionBroker WebAccessServer SessionHost" -ForegroundColor Green
 New-RDSessionDeployment -ConnectionBroker  "$myFQDN" -WebAccessServer "$myFQDN" -SessionHost "$myFQDN"
 
